980bab700fd0d79f39ae061174e3419624b4f4a0
AdrianForsythe <adrian.forsythe@ebc.uu.se>
2025-03-19 10:51:58 +0100

    properly handle environment variables and exclude comments from env

--- mas-worker.sh
+++ mas-worker.sh
@@ -6,4 +6,4 @@ cd "$wd" || exit
 
 source .env
 
-$CONDA_EXE run -n mas-worker --cwd "$wd" env $(grep -v '^#' .env | xargs) CELERY_WORKER=TRUE celery multi "$subcmd" mas-worker --hostname=host -A MAS -B --concurrency=5 --pidfile="$MAS_WORKER_PID_FILE" --logfile="$MAS_WORKER_LOG";
+$CONDA_EXE run -n mas-worker --cwd "$wd" env $(grep -v '^#' .env | sed '/^[[:space:]]*$/d' | tr '\n' ' ') CELERY_WORKER=TRUE celery multi "$subcmd" mas-worker --hostname=host -A MAS -B --concurrency=5 --pidfile="$MAS_WORKER_PID_FILE" --logfile="$MAS_WORKER_LOG"
