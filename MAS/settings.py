e663cb18c51ee5b59c0423c5a5dadf0614bc1910
AdrianForsythe <adrian.forsythe@ebc.uu.se>
2025-05-27 13:28:31 +0200

    update swissprot path

--- MAS/settings.py
+++ MAS/settings.py
@@ -117,7 +117,7 @@ TERMINASE_DATABASE = "/home/daemon/MAS/databases/terminase/terminase_db"
 INTERNAL_NUCLEOTIDE_DB_PATH = "/mnt/sda/johannesson_lab/adrian/bin/MAS/internal_db/nucleotide"
 NUCLEOTIDE_DATABASE = "/mnt/sda/johannesson_lab/adrian/bin/MAS/internal_db/nucleotide.db"
 PROTEIN_DATABASE = "/mnt/sda/johannesson_lab/adrian/bin/MAS/internal_db/internal.db"
-
+SWISSPROT_DIR = "/mnt/sda/johannesson_lab/swissprot"
 GIT_DIR = os.path.join(BASE_DIR, ".git")
 
 if os.getenv('CELERY_WORKER') == 'TRUE':
