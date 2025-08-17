f3cf8438a70d84c912c47bf5f2ae861ec86ab35f
AdrianForsythe <adrian.forsythe@ebc.uu.se>
2025-08-17 19:16:00 +0200

    registered new models

--- starship/admin.py
+++ starship/admin.py
@@ -6,9 +6,22 @@ from django.contrib import admin
 from . import models as starship_models
 
 
-admin.site.register(starship_models.Starship)
 admin.site.register(starship_models.Feature)
 admin.site.register(starship_models.Annotation)
+
+# New models from SQLite schema
+admin.site.register(starship_models.Accessions)
+admin.site.register(starship_models.Ships)
+admin.site.register(starship_models.Captains)
+admin.site.register(starship_models.Taxonomy)
+admin.site.register(starship_models.Genome)
+admin.site.register(starship_models.Papers)
+admin.site.register(starship_models.FamilyNames)
+admin.site.register(starship_models.StarshipFeatures)
+admin.site.register(starship_models.NavisHaplotype)
+admin.site.register(starship_models.Gff)
+admin.site.register(starship_models.JoinedShips)
+
 # admin.site.register(starship_models.HHSearch_Result)
 # admin.site.register(starship_models.Blastp_Result)
 # admin.site.register(starship_models.RPSBlast_Result)
